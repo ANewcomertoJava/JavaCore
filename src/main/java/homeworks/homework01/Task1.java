b161f7e7abd7f197cd1875cea4ed1b41dc21f707
ANewcomertoJava <iit8610@gmail.com>
2024-12-22 21:31:06 +0300

    homework05Addition добавлены домашние задания с первой по пятую

---
+++ src/main/java/homeworks/homework01/Task1.java
@@ -0,0 +1,13 @@
+package homeworks.homework01;
+
+public class Task1 {
+ public static void main(String[] args) {
+  int a = 8;
+  int b = 20;
+  int c = 34;
+  int d = 99;
+
+  // Выводим числа с тремя пробелами между ними
+  System.out.println(a + " " + b + " " + c + " " + d);
+ }
+}
---
+++ src/main/java/homeworks/homework03/App.java
@@ -0,0 +1,15 @@
+package homeworks.homework03;
+
+public class App {
+        public static void main(String[] args) {
+        // создание экземпляров класса TelevisorUpdate
+        Televisor tv1 = new Televisor("Samsung", "UE50DU7100UXRU", 50);
+        Televisor tv2 = new Televisor("Sony", "KD-55X75K AF1", 54.6);
+        Televisor tv3 = new Televisor("Thoshiba", "100Z670NE", 100);
+
+        System.out.println(tv1.toString());
+        System.out.println(tv2.toString());
+        System.out.println(tv3.toString());
+    }
+}
+
