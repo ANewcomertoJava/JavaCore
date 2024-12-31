5a6aec5254341e386949740e28d7ce733a42f5d1
ANewcomertoJava <iit8610@gmail.com>
2024-12-31 19:46:09 +0300

    Домашнее задание по теме «Работа с классами и объектами. IO» 1. Скопировано ДЗ6 2. Создан try catch 3. Создан буферридер 4. Исправлена небольшая опечатка 5. Создан записыватель

--- src/main/java/homeworks/homework08/product/App.java
+++ src/main/java/homeworks/homework08/product/App.java
@@ -2,27 +2,36 @@ package homeworks.homework08.product;
 
 import homeworks.homework08.person.Person;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class App {
     public static void main(String[] args) {
 
-
+        // для считывания из файла
         try {
+            String string;
             FileReader fileReader = new FileReader("src/main/java/homeworks/homework08/product/input.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("src/main/java/homeworks/homework08/product/output.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             while (bufferedReader.ready()) {
-                System.out.println(bufferedReader.readLine());
+                string = bufferedReader.readLine();
+                bufferedWriter.write(string + "\r\n");
+
             }
 
-
+            bufferedReader.close();
             fileReader.close();
+            bufferedWriter.flush();
+            bufferedWriter.close();
+
+
         } catch (IOException ioException){
             ioException.printStackTrace();
         }
+
+
 
 
     }
